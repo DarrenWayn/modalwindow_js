8052135217971c1564889891b49250b2c3412927
Darren Wayn <darrenwayn98@gmail.com>
2022-07-01 18:13:10 +0700

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-'use strict'
+'use strict';
 // declaration
-const modal =  document.querySelector('.modal')
-const overlay = document.querySelector('.overlay')
-const btnCloseModal = document.querySelector('.close-modal')
-const btnsOpenModal = document.querySelectorAll('.show-modal')
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const closeModal = function() {
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
-}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-const openModal = function() {
-    modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnsOpenModal.length; i++) 
-    btnsOpenModal[i].
-    addEventListener('click', openModal)
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click', closeModal)
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    //if the modal doesn't contain the hidden class then close the modal
+    closeModal();
+  }
+});
